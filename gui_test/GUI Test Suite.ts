50ea29e2e67c62a5917dd79320a2b37d1d5520c5
Joep Laarhoven <laarhoven.joep@gmail.com>
2021-12-02 12:55:15 +0100

    add test suite path

---
+++ gui_test/GUI Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>GUI Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>54b921ed-3b2d-4fd0-a474-f811b759d7a9</testSuiteGuid>
+   <testCaseLink>
+      <guid>e26ebe33-1074-4231-ae65-c8e6591d1ea7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GUI Test case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
